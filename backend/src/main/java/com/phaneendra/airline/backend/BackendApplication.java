8e69e9510d4624bedf954399a2b1ea5a68ac32fb
Phaneendra Balla <phaneendraballa16@gmail.com>
2026-07-06 12:10:08 +0530

    Implement to Application and database with time zone to  Asia/Kolkata

--- backend/src/main/java/com/phaneendra/airline/backend/BackendApplication.java
+++ backend/src/main/java/com/phaneendra/airline/backend/BackendApplication.java
@@ -1,13 +1,22 @@
 package com.phaneendra.airline.backend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class BackendApplication {
-
+	
+	@PostConstruct
+    public void init() {
+        // This forces Spring Boot application to use India's clock time
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
-
 }
